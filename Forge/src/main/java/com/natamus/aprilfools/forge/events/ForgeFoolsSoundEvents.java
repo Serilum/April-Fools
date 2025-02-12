afe84662ae4133f155ba652e6bff571c6740a560
Rick South <rick@natamus.com>
2025-02-12 23:45:49 +0100

    (v1.6) (Forge & Neo) Removed redundant event register annotations. [+1]

--- Forge/src/main/java/com/natamus/aprilfools/forge/events/ForgeFoolsSoundEvents.java
+++ Forge/src/main/java/com/natamus/aprilfools/forge/events/ForgeFoolsSoundEvents.java
@@ -4,12 +4,10 @@ import com.natamus.aprilfools.events.FoolsSoundEvents;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.sound.PlaySoundEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 
-@EventBusSubscriber(value = Dist.CLIENT)
 public class ForgeFoolsSoundEvents {
 	@SubscribeEvent
-	public void onSoundEvent(PlaySoundEvent e) {
+	public static void onSoundEvent(PlaySoundEvent e) {
 		if (!FoolsSoundEvents.onSoundEvent(e.getEngine(), e.getOriginalSound())) {
 			e.setSound(null);
 		}
