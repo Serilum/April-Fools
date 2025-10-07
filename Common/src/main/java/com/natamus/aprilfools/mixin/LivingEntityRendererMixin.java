bde4787e5e13726b34b11567d297559a36601f91
Rick South <rick@natamus.com>
2025-10-08 01:51:35 +0200

    April Fools source code for 1.21.10.

---
+++ Common/src/main/java/com/natamus/aprilfools/mixin/LivingEntityRendererMixin.java
@@ -0,0 +1,35 @@
+package com.natamus.aprilfools.mixin;
+
+import com.mojang.blaze3d.vertex.PoseStack;
+import com.natamus.aprilfools.config.ConfigHandler;
+import com.natamus.aprilfools.util.Util;
+import net.minecraft.client.model.EntityModel;
+import net.minecraft.client.renderer.SubmitNodeCollector;
+import net.minecraft.client.renderer.entity.EntityRenderer;
+import net.minecraft.client.renderer.entity.EntityRendererProvider;
+import net.minecraft.client.renderer.entity.LivingEntityRenderer;
+import net.minecraft.client.renderer.entity.RenderLayerParent;
+import net.minecraft.client.renderer.entity.state.LivingEntityRenderState;
+import net.minecraft.client.renderer.state.CameraRenderState;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.Pose;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(value = LivingEntityRenderer.class, priority = 1001)
+public abstract class LivingEntityRendererMixin<T extends LivingEntity, S extends LivingEntityRenderState, M extends EntityModel<? super S>> extends EntityRenderer<T, S> implements RenderLayerParent<S, M> {
+	protected LivingEntityRendererMixin(EntityRendererProvider.Context $$0) { super($$0); }
+
+	@Inject(method = "submit(Lnet/minecraft/client/renderer/entity/state/LivingEntityRenderState;Lcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/SubmitNodeCollector;Lnet/minecraft/client/renderer/state/CameraRenderState;)V", at = @At(value = "INVOKE", target = "Lcom/mojang/blaze3d/vertex/PoseStack;scale(FFF)V", ordinal = 0))
+	public void render(LivingEntityRenderState livingEntityRenderState, PoseStack poseStack, SubmitNodeCollector submitNodeCollector, CameraRenderState $$3, CallbackInfo ci) {
+		if (!Util.areAprilFoolsFeaturesEnabled() || !ConfigHandler.makePlayersSleepWithFeetOnPillow) {
+			return;
+		}
+
+		if (livingEntityRenderState.hasPose(Pose.SLEEPING)) {
+			poseStack.translate(0.0F, 1.2F, 0.0F);
+		}
+	}
+}
