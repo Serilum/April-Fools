587f153ff2d768c8180c5b8003b8bc418824bfe4
Rick South <rick@natamus.com>
2026-05-01 23:51:04 +0200

    (v1.9) (26.1.2 NeoForge) Updated to work with NeoForge version 26.1.2.21-beta and above.

--- NeoForge/src/main/java/com/natamus/aprilfools/neoforge/events/NeoForgeFoolsBlockEvents.java
+++ NeoForge/src/main/java/com/natamus/aprilfools/neoforge/events/NeoForgeFoolsBlockEvents.java
@@ -4,11 +4,11 @@ import com.natamus.aprilfools.events.FoolsBlockEvents;
 import com.natamus.collective.functions.WorldFunctions;
 import net.minecraft.world.level.Level;
 import net.neoforged.bus.api.SubscribeEvent;
-import net.neoforged.neoforge.event.level.BlockEvent;
+import net.neoforged.neoforge.event.level.block.BreakBlockEvent;
 
 public class NeoForgeFoolsBlockEvents {
 	@SubscribeEvent
-	public static void onBlockBreak(BlockEvent.BreakEvent e) {
+	public static void onBlockBreak(BreakBlockEvent e) {
 		Level level = WorldFunctions.getWorldIfInstanceOfAndNotRemote(e.getLevel());
 		if (level == null) {
 			return;
