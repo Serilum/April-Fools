62426b568749c6621ff8bd586884da9eb60cef86
Rick South <38502394+ricksouth@users.noreply.github.com>
2026-06-07 19:38:56 +0200

    (v2.0) Updated the mod's text to use Collective's new translation system, which adds many languages alongside English! [+1]

--- Common/src/main/java/com/natamus/aprilfools/util/Reference.java
+++ Common/src/main/java/com/natamus/aprilfools/util/Reference.java
@@ -3,6 +3,6 @@ package com.natamus.aprilfools.util;
 public class Reference {
 	public static final String MOD_ID = "aprilfools";
 	public static final String NAME = "April Fools";
-	public static final String VERSION = "1.9";
+	public static final String VERSION = "2.0";
 	public static final String ACCEPTED_VERSIONS = "[26.1.2]";
 }
